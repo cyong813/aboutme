3453249d7db1879529b5ca9247569391e5c76c31
cyong813 <cy986@nyu.edu>
2018-09-05 08:53:47 -0400

    add particles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { Parallax, Background } from 'react-parallax';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import Scroll from './Scroll'
+import Particles from 'react-particles-js';
 
 class Header extends Component {
   constructor(props) {
@@ -21,14 +22,66 @@ class Header extends Component {
           backgroundImage: 'linear-gradient(to bottom, #9c9bbf 0%, #9ebcd5 60%)'}}
         >
           <div style={{ height: '650px'}}> 
+          <Particles
+                params={{
+                  "particles": {
+                      "number": {
+                          "value": 160,
+                          "density": {
+                              "enable": false
+                          }
+                      },
+                      "size": {
+                          "value": 3,
+                          "random": true,
+                          "anim": {
+                              "speed": 4,
+                              "size_min": 0.3
+                          }
+                      },
+                      "line_linked": {
+                          "enable": false
+                      },
+                      "move": {
+                          "random": true,
+                          "speed": 1,
+                          "direction": "top",
+                          "out_mode": "out"
+                      }
+                  },
+                  "interactivity": {
+                      "events": {
+                          "onhover": {
+                              "enable": true,
+                              "mode": "bubble"
+                          },
+                          "onclick": {
+                              "enable": true,
+                              "mode": "repulse"
+                          }
+                      },
+                      "modes": {
+                          "bubble": {
+                              "distance": 250,
+                              "duration": 2,
+                              "size": 0,
+                              "opacity": 0
+                          },
+                          "repulse": {
+                              "distance": 400,
+                              "duration": 4
+                          }
+                      }
+                  }
+              }} />
             <div id='header'>
               <h1>Corinna</h1>
               <p>Computer Science & Cybersecurity</p>
               <div id='down-icon'>
-              <Scroll type="id" element="intro">
-                <a href='#'>
-                  <FontAwesomeIcon icon={faCaretDown} size="lg" color="white"/>
-                </a>
+                <Scroll type="id" element="intro">
+                  <a href='#'>
+                    <FontAwesomeIcon icon={faCaretDown} size="lg" color="white"/>
+                  </a>
                 </Scroll>
               </div>
             </div>
