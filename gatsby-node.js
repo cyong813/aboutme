4c497dfe050720a1d6369466a7159c0e263a1c3a
cyong813 <cy986@nyu.edu>
2018-09-05 21:25:51 -0400

    particle fix 2

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,9 @@
 const path = require("path")
+exports.modifyWebpackConfig = ({ config, stage }) => {
+    if (stage === 'build-html') {
+      config.loader('null', {
+        test: /react-particles-js/,
+        loader: 'null-loader'
+      })
+    }
+  }
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,82 +12,75 @@ class Header extends Component {
   }
 
   render() {
-    return (
-      <Parallax
-        strength={100}
-        style={{
-          backgroundImage: '-webkit-gradient(linear, left top, left bottom, from(#9c9bbf), color-stop(60%, #9ebcd5))',
-          backgroundImage: '-webkit-linear-gradient(top, #9c9bbf 0%, #9ebcd5 60%)',
-          backgroundImage: '-o-linear-gradient(top, #9c9bbf 0%, #9ebcd5 60%)',
-          backgroundImage: 'linear-gradient(to bottom, #9c9bbf 0%, #9ebcd5 60%)'}}
-        >
-          <div style={{ height: '650px'}}> 
-          <Particles
-                params={{
-                  "particles": {
-                      "number": {
-                          "value": 160,
-                          "density": {
-                              "enable": false
-                          }
-                      },
-                      "size": {
-                          "value": 3,
-                          "random": true,
-                          "anim": {
-                              "speed": 4,
-                              "size_min": 0.3
-                          }
-                      },
-                      "line_linked": {
-                          "enable": false
-                      },
-                      "move": {
-                          "random": true,
-                          "speed": 1,
-                          "direction": "top",
-                          "out_mode": "out"
-                      }
-                  },
-                  "interactivity": {
-                      "events": {
-                          "onhover": {
-                              "enable": true,
-                              "mode": "bubble"
-                          },
-                          "onclick": {
-                              "enable": true,
-                              "mode": "repulse"
-                          }
-                      },
-                      "modes": {
-                          "bubble": {
-                              "distance": 250,
-                              "duration": 2,
-                              "size": 0,
-                              "opacity": 0
-                          },
-                          "repulse": {
-                              "distance": 400,
-                              "duration": 4
-                          }
-                      }
-                  }
-              }} />
-            <div id='header'>
-              <h1>Corinna</h1>
-              <p>Computer Science & Cybersecurity</p>
-              <div id='down-icon'>
-                <Scroll type="id" element="intro">
-                  <a href='#'>
-                    <FontAwesomeIcon icon={faCaretDown} size="lg" color="white"/>
-                  </a>
-                </Scroll>
-              </div>
+    if (typeof window !== 'undefined') {
+        return (
+            <div className='header-particles'> 
+            <Particles
+                    params={{
+                    "particles": {
+                        "number": {
+                            "value": 160,
+                            "density": {
+                                "enable": false
+                            }
+                        },
+                        "size": {
+                            "value": 3,
+                            "random": true,
+                            "anim": {
+                                "speed": 4,
+                                "size_min": 0.3
+                            }
+                        },
+                        "line_linked": {
+                            "enable": false
+                        },
+                        "move": {
+                            "random": true,
+                            "speed": 1,
+                            "direction": "top",
+                            "out_mode": "out"
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "bubble"
+                            },
+                            "onclick": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        },
+                        "modes": {
+                            "bubble": {
+                                "distance": 250,
+                                "duration": 2,
+                                "size": 0,
+                                "opacity": 0
+                            },
+                            "repulse": {
+                                "distance": 400,
+                                "duration": 4
+                            }
+                        }
+                    }
+                }} />
+                <div id='header'>
+                <h1>Corinna</h1>
+                <p>Computer Science & Cybersecurity</p>
+                <div id='down-icon'>
+                    <Scroll type="id" element="intro">
+                    <a href='#'>
+                        <FontAwesomeIcon icon={faCaretDown} size="lg" color="white"/>
+                    </a>
+                    </Scroll>
+                </div>
+                </div>
             </div>
-          </div>
-      </Parallax>
-    )
+        )
+    } return null
   }
   
 }
